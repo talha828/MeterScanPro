f2f85f4ce08a400bfc53b2dff5b8aad3ffa0efeb
talha828 <talha.developer.01@gmail.com>
2023-10-06 16:42:10 +0500

    quick fix

---
+++ lib/constant/constant.dart
@@ -0,0 +1,4 @@
+ import 'dart:ui';
+
+const themeColor1 = Color(0xff23254C);
+const themeColor2 = Color(0xffD4AD4E);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meter_scan/constant/constant.dart';
 import 'package:meter_scan/view/splash_screen/splash_screen.dart';
 
 void main() {
@@ -13,12 +15,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.white,
+        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme,),
+        primaryTextTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
+        primarySwatch: createMaterialColor(themeColor1),
+        buttonTheme:const ButtonThemeData(
+          buttonColor: themeColor1,
+        ),
         useMaterial3: true,
       ),
       home:const SplashScreen(),
     );
+  }
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    final swatch = <int, Color>{};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    strengths.forEach((strength) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    });
+    return MaterialColor(color.value, swatch);
   }
 }
